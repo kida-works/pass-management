836f1385079edfaffc7a9c63734f24dd70c49021
kida-works <kida.works.1234@gmail.com>
2022-07-25 00:41:15 +0900

    first commit

---
+++ src/components/pages/Home.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Home:React.FC = () => {
+  return(
+    <div>
+      <header>
+        <h1>
+
+        </h1>
+      </header>
+      <main>
+        
+      </main>
+    </div>
+  )
+}
+
+export default Home;
