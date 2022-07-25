463708a72d29146b3acee32a61658f3d78968ef4
kida-works <kida.works.1234@gmail.com>
2022-07-25 23:41:08 +0900

    add

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,18 +1,30 @@
-import React from 'react';
+import React, { useEffect } from "react";
 
-const Home:React.FC = () => {
-  return(
+const Home: React.FC = () => {
+  const titleObject = [
+    {
+      mainTitle: "Google",
+      subTitle: "google アカウント",
+      pass: "test1234",
+    },
+    {
+      mainTitle: "Microsoft",
+      subTitle: "Microsoft アカウント",
+      pass: "test5678",
+    },
+  ];
+  
+  useEffect(() => {
+    console.log("test");
+  }, []);
+  return (
     <div>
       <header>
-        <h1>
-
-        </h1>
+        <h1>Home</h1>
       </header>
-      <main>
-        
-      </main>
+      <main></main>
     </div>
-  )
-}
+  );
+};
 
 export default Home;
