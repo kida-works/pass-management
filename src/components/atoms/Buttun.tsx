6b62881dd15d1aaeca4506391028f17400dd6516
kida-works <kida.works.1234@gmail.com>
2022-08-13 23:14:53 +0900

    add

--- src/components/atoms/Buttun.tsx
+++ src/components/atoms/Buttun.tsx
@@ -1,14 +1,31 @@
 import React from "react";
+import styled from "styled-components";
 
 type MyFunctionType = (value: boolean | string) => void;
-interface PropsType  {
-  isFlagFunc: MyFunctionType | React.Dispatch<React.SetStateAction<boolean>>
-  text: string
-  argument: boolean
+interface PropsType {
+  isFlagFunc: MyFunctionType | React.Dispatch<React.SetStateAction<boolean>>;
+  text: string;
+  argument: boolean;
 }
 
 const Button: React.FC<PropsType> = (props) => {
-  return <button onClick={()=>props.isFlagFunc(props.argument)}>{props.text}</button>;
+  return (
+    <StyledButton onClick={() => props.isFlagFunc(props.argument)}>
+      {props.text}
+    </StyledButton>
+  );
 };
+
+const StyledButton = styled.button`
+  color: #fff;
+  width: 60px;
+  height: 60px;
+  border-radius: 50%;
+  box-shadow: 0 10px 10px 0 rgba(0, 0, 0, 0.5);
+  background: #2196f3;
+  position: fixed;
+  top: 20px;
+  right: 20px;
+`;
 
 export default Button;
